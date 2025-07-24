b6c298c7a16239e5268f81095b5ab20f591f5385
hamedsharifi <handroidsharify@gmail.com>
2025-07-24 12:01:40 +0330

    @EnableScheduling

--- infrastructure/src/main/java/ir/welunch/app/WalletChallengeApplication.java
+++ infrastructure/src/main/java/ir/welunch/app/WalletChallengeApplication.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import ir.welunch.core.annotation.CoreComponent;
 import ir.welunch.core.annotation.UseCase;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ir.welunch"},
         includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
                 classes = {UseCase.class, CoreComponent.class}))
+@EnableScheduling
 public class WalletChallengeApplication {
     public static void main(String[] args) {
         SpringApplication.run(WalletChallengeApplication.class, args);
