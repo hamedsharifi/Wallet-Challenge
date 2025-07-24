bd298db1b9361752da0b286e3229de9b710871f5
hamedsharifi <handroidsharify@gmail.com>
2025-07-24 10:26:11 +0330

    provide controllers and use of use cases

--- infrastructure/src/main/java/ir/welunch/app/controller/WalletController.java
+++ infrastructure/src/main/java/ir/welunch/app/controller/WalletController.java
@@ -1,8 +1,12 @@
 package ir.welunch.app.controller;
 
+import ir.welunch.core.dto.AddMoneyToWalletRequest;
+import ir.welunch.core.dto.AddMoneyToWalletResponse;
+import ir.welunch.core.dto.GetWalletBalanceRequest;
+import ir.welunch.core.dto.GetWalletBalanceResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ir.welunch.core.usecase.AddMoneyToWalletUseCase;
 import ir.welunch.core.usecase.GetWalletBalanceByUserIdUseCase;
 
@@ -15,4 +19,13 @@ public class WalletController {
     private final AddMoneyToWalletUseCase addMoneyToWalletUseCase;
 
 
+    @GetMapping("/get-balance")
+    public ResponseEntity<GetWalletBalanceResponse> getBalance(GetWalletBalanceRequest request) {
+        return ResponseEntity.ok(getWalletBalanceByUserIdUseCase.execute(request));
+    }
+
+    @PostMapping("/charge")
+    public ResponseEntity<AddMoneyToWalletResponse> addToCart(@RequestBody AddMoneyToWalletRequest request) {
+        return ResponseEntity.ok(addMoneyToWalletUseCase.execute(request));
+    }
 }
